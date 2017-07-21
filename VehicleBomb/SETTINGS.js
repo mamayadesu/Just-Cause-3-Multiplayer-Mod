960cc58eb2179862d308b223659f9784028712b9
Semyon Semyonov <mrtech@dsquares.ru>
2017-07-21 21:20:37 +0500

    Create SETTINGS.js

---
+++ VehicleBomb/SETTINGS.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = {
+    forceKillPlayerIfHasBlownUp: false, // !!! NOT supported yet. Sorry.
+    jumpVehicleOnOneMeter: true // There is a probability that the vehicle won't explode if she is at rest. Also will blow up until there is any interaction to it.
+};
